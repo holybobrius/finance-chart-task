6c1020b523883e98b6b9010a5482ad2973858376
Ilya Povarov <ilya.povaroff@gmail.com>
2021-11-09 10:05:30 +0300

    boilerplate files removed

--- src/ChartComponent.js
+++ src/ChartComponent.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Chart from './Chart';
 import { getData } from "./utils"
 
-import { TypeChooser } from "react-stockcharts/lib/helper";
-
 const ChartComponent = () => {
   const [data, setData] = useState()
 
